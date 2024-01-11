0b42021826054b11f10faedb0e55d9caca1035b4
IdiJr <idialijnr@gmail.com>
2024-01-11 13:29:49 +0000

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+filtered_logger module
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ returns the log message obfuscated """
+    return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
+                  lambda match: match.group(1) + "=" + redaction
+                  if match.group(1) in fields else match.group(0), message)
