e6386c19dc7e3935b49a28f4588e8603d2642f95
IdiJr <idialijnr@gmail.com>
2024-01-26 04:32:20 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -47,3 +49,21 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database by given criteria
+
+        Args:
+            **kwargs: Arbitrary keyword arguments representing filter criteria
+
+        Returns:
+            User: The first user found based on the filter criteria
+
+        Raises:
+            NoResultFound: If no results are found
+            InvalidRequestError: If wrong query arguments are passed
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
