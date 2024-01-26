cc0e9fe2a3a619889eca79156cbfefb215e5b22e
IdiJr <idialijnr@gmail.com>
2024-01-26 04:15:53 +0000

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 """
-User module with SQLAlchemy model
+User model
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     User model for the users table
+    Args:
+        Base (class): declarative base from sqlalchemy
     """
     __tablename__ = 'users'
 
