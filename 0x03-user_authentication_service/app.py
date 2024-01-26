820ddcf4bb0d80ec36dd8e86ec33b62a97dda205
IdiJr <idialijnr@gmail.com>
2024-01-26 06:12:23 +0000

    modified code for log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,12 +44,16 @@ def login() -> str:
     """
     email = request.form.get('email')
     password = request.form.get('password')
+
     valid_login = AUTH.valid_login(email, password)
+
     if not valid_login:
         abort(401)
+
     session_id = AUTH.create_session(email)
-    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie('session_id', session_id)
+    
     return response
 
 
